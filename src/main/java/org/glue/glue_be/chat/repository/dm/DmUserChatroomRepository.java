4421fd7483879356e901e6f4a198e8e8eae820ed
kim-minkyoung <decomin02@pusan.ac.kr>
2025-04-16 23:48:32 +0900

    feat: repository 추가

---
+++ src/main/java/org/glue/glue_be/chat/repository/dm/DmUserChatroomRepository.java
@@ -0,0 +1,24 @@
+package org.glue.glue_be.chat.repository.dm;
+
+import org.glue.glue_be.chat.entity.dm.DmChatRoom;
+import org.glue.glue_be.chat.entity.dm.DmUserChatroom;
+import org.glue.glue_be.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface DmUserChatroomRepository extends JpaRepository<DmUserChatroom, Long> {
+    List<DmUserChatroom> findByDmChatRoom(DmChatRoom dmChatRoom);
+
+    @Query("SELECT uc.user.userId FROM DmUserChatroom uc WHERE uc.dmChatRoom.id = :dmChatRoomId")
+    List<Long> findUserIdsByDmChatRoomId(@Param("dmChatRoomId") Long dmChatRoomId);
+
+    void deleteByDmChatRoomAndUser(DmChatRoom dmChatRoom, User user);
+
+    Optional<Object> findByDmChatRoomAndUser(DmChatRoom dmChatRoom, User sender);
+}
