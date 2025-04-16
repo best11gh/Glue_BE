e4f3362b7c2df8470de700bde8086edc9d6a9d92
kim-minkyoung <decomin02@pusan.ac.kr>
2025-04-16 23:44:09 +0900

    feat: WebSocketConfig.java 추가

--- src/main/java/org/glue/glue_be/chat/config/WebSocketConfig.java
+++ src/main/java/org/glue/glue_be/chat/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // STOMP 연결 엔드포인트 등록
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
-                .withSockJS(); // SockJS 지원 추가
+                .withSockJS(); // SockJS 지원 추가 (WebSocket을 지원하지 않는 브라우저에서도 실시간 통신이 가능하도록 하기 위함)
     }
 
     @Override
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 메시지를 구독하는 엔드포인트 prefix 설정
         registry.enableSimpleBroker("/topic", "/queue");
 
-        // 메시지를 발행하는 엔드포인트 prefix 설정
+        // 메시지를 발행하는 엔드포인트 prefix(접두사) 설정
         // /topic: 1:N 그룹 채팅
         // /queue: 1:1 개인 채팅
         registry.setApplicationDestinationPrefixes("/app");
