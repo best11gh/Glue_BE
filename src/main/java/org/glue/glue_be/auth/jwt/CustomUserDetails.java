cf486af0db6b39ad2dca60c2ef4a22d32348aedd
Lee Si Woong <silverttthin@naver.com>
2025-05-04 17:02:42 +0900

    feat: 로그인 유저의 정보들을 담아 컨트롤러에서 뽑아 쓸 객체 생성 - 지정된 인터페이스에 의거한 커스텀 객체 만들고 유저 정보는 id만 삽입 - 추후 닉네임 등의 다른 정보 추가는 협의를 통해 구현 가능

---
+++ src/main/java/org/glue/glue_be/auth/jwt/CustomUserDetails.java
@@ -0,0 +1,42 @@
+package org.glue.glue_be.auth.jwt;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+// Principal에 들어갈 유저 정보를 담고있는 객체
+public class CustomUserDetails implements UserDetails {
+
+	// 인증 객체에 들어갈 유저 정보들은 추후 협의하며 추가해갈 수 있음
+	private final Long userOauthId;
+
+	public CustomUserDetails(Long userOauthId) { this.userOauthId = userOauthId; }
+
+	// 인터페이스 따라가기에 함수명은 getUsername이지만 실제론 유저의 id를 리턴
+	@Override
+	public String getUsername() { return userOauthId.toString(); }
+
+
+
+	//--------- 하단 메서드들은 쓸모없지만 인터페이스 때문에 일단 구현은 필수라 적당히 넣어둔 것
+
+
+	// 권한 로직이 아직 존재하지 않으므로 빈 리스트로 처리
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// 유저 객체에 넣을 권한 리스트 -> 아직 권한 세팅 안해놔서 빈 리스트
+		List<GrantedAuthority> authorities = Collections.emptyList();
+		return authorities;
+	}
+
+
+	// 우린 소셜로그인 사용하기 때문에 디비에 유저의 암호를 저장하지 않아 아마 구현할 일이 없을 메서드
+	@Override
+	public String getPassword() {
+		return "";
+	}
+
+}
