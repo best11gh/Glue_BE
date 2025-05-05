b2394ac812031bd515a32d18d9f395d1be04dd12
Lee Si Woong <silverttthin@naver.com>
2025-05-05 21:46:03 +0900

    refactor: 카카오 인증로직 조금 손보기 - 로그인에 검증 추가 - 회원가입은 바뀌어야 할 속성이 꽤 많아 그냥 방치 - controller에서 안쓰는 kakaoservice 빈 삭제

--- src/main/java/org/glue/glue_be/auth/dto/request/KakaoSignInRequestDto.java
+++ src/main/java/org/glue/glue_be/auth/dto/request/KakaoSignInRequestDto.java
@@ -1,12 +1,14 @@
 package org.glue.glue_be.auth.dto.request;
 
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 
 @Getter
 public class KakaoSignInRequestDto {
 
+	@NotEmpty(message = "카카오 발급 토큰값은 필수입니다")
 	String kakaoToken;
 
 }
