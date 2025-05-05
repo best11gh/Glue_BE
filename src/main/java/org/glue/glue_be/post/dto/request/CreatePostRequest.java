039cd48ad50c1354622ae1ba4061cf9d3fb35fed
Lee Si Woong <silverttthin@naver.com>
2025-05-05 21:08:07 +0900

    feat: 게시글 생성 reqDto에 notnull/blank 처리 - 장소 관련은 기획 미확정이라 별 처리안함

--- src/main/java/org/glue/glue_be/post/dto/request/CreatePostRequest.java
+++ src/main/java/org/glue/glue_be/post/dto/request/CreatePostRequest.java
@@ -1,9 +1,9 @@
 package org.glue.glue_be.post.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
 
@@ -16,12 +16,15 @@ public class CreatePostRequest {
 	@Getter
 	public static class MeetingDto {
 
+		@NotBlank(message = "모임 제목은 필수값입니다.")
 		private String meetingTitle;
 
+		@NotNull(message = "카테고리 id는 필수값입니다")
 		private Integer categoryId;
 
 		private String meetingPlaceName;
 
+		@NotNull(message = "모임일시는 필수값입니다")
 		@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		private LocalDateTime meetingTime;
 
@@ -29,16 +32,20 @@ public class CreatePostRequest {
 
 		private Double meetingPlaceLongitude;
 
+		@NotNull(message = "언어 id는 필수값입니다")
 		private Integer languageId;
 
+		@NotNull(message = "최대 참가자수는 필수값입니다")
 		private Integer maxParticipants;
 	}
 
 	@Getter
 	public static class PostDto {
 
+		@NotBlank(message = "게시글 제목은 필수값입니다.")
 		private String title;
 
+		@NotBlank(message = "게시글 내용은 필수값입니다.")
 		private String content;
 
 	}
