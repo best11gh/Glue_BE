3225d9cdd74cb53d9269a1ea4080cd45ab7c9eac
Gahyeon <best11gh@naver.com>
2025-05-06 14:15:53 +0900

    feat(fcm): controller, service 구현

---
+++ src/main/java/org/glue/glue_be/util/fcm/controller/FcmController.java
@@ -0,0 +1,38 @@
+package org.glue.glue_be.util.fcm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.glue.glue_be.common.response.BaseResponse;
+import org.glue.glue_be.util.fcm.dto.FcmSendDto;
+import org.glue.glue_be.util.fcm.dto.MultiFcmSendDto;
+import org.glue.glue_be.util.fcm.response.FcmResponseStatus;
+import org.glue.glue_be.util.fcm.service.FcmService;
+import org.springframework.web.bind.annotation.*;
+
+// 테스트 용
+@RestController
+@Slf4j
+@RequestMapping("/api/fcm")
+public class FcmController {
+
+    final FcmService fcmService;
+
+    public FcmController(FcmService fcmService) {
+        this.fcmService = fcmService;
+    }
+
+    // 단일 기기로 fcm 발송
+    @PostMapping("/single")
+    public BaseResponse<Void> sendSingle(@RequestBody FcmSendDto dto) {
+        fcmService.sendMessageByFcm(dto);
+        return new BaseResponse<>(FcmResponseStatus.FCM_SEND_SUCCESS);
+    }
+
+    // 여러 기기로 fcm 발송
+    @PostMapping("/multi")
+    public BaseResponse<Void> sendMulti(@RequestBody MultiFcmSendDto dto) {
+        fcmService.sendMultiMessage(dto);
+        return new BaseResponse<>(FcmResponseStatus.FCM_MULTICAST_SUCCESS);
+    }
+
+
+}
