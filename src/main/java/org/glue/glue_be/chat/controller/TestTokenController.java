882cfa69fb0050b0c61f637290e1e164ee1417a3
kim-minkyoung <decomin02@pusan.ac.kr>
2025-05-10 20:47:27 +0900

    test: JwtTokenProvider 변경 후 토큰 생성/파싱 테스트코드 축가

--- src/main/java/org/glue/glue_be/chat/controller/TestTokenController.java
+++ src/main/java/org/glue/glue_be/chat/controller/TestTokenController.java
@@ -1,7 +1,9 @@
 package org.glue.glue_be.chat.controller;
 
 import org.glue.glue_be.auth.jwt.JwtTokenProvider;
+import org.glue.glue_be.auth.jwt.JwtValidationType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,5 +31,25 @@ public class TestTokenController {
         String token2 = jwtTokenProvider.generateToken(auth3);
 
         return "사용자 1번 토큰: " + token1 + "\n\n사용자 3번 토큰: " + token2;
+    }
+
+    @GetMapping("/api/test/parse-tokens")
+    public ResponseEntity<String> parseHardcodedTokens() {
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3NDY4NzcwODIsImV4cCI6MTc0Njk2MzQ4MiwibWVtYmVySWQiOiJhZDFmYTkyMy01NzdhLTQ0MzYtYWM2NS1mYjYzNzZiNDk0ZjIifQ.5IsDw4dGviy51WQkA0m_ztg_N10A4YOV_3rhF_elmDNP1XmzWv_0xj7OjvgotZT0KLGNENxPdY8uG4pqimUiyA";
+        String results = "";
+
+        try {
+            JwtValidationType validationType1 = jwtTokenProvider.validateToken(token);
+            if (validationType1 == JwtValidationType.VALID_JWT) {
+                Authentication auth1 = jwtTokenProvider.getAuthentication(token);
+                String uuid1 = auth1.getName();
+                results = uuid1;
+            }
+
+            return ResponseEntity.ok(results);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getLocalizedMessage());
+        }
     }
 }
