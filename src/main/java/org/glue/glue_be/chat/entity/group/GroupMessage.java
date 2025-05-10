809de1b2f92cc51e6472cb869ae0a078ef25b645
kim-minkyoung <decomin02@pusan.ac.kr>
2025-05-10 20:24:18 +0900

    feat: 그룹채팅 관련 엔티티 추가

---
+++ src/main/java/org/glue/glue_be/chat/entity/group/GroupMessage.java
@@ -0,0 +1,53 @@
+package org.glue.glue_be.chat.entity.group;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.glue.glue_be.common.BaseEntity;
+import org.glue.glue_be.meeting.entity.Meeting;
+import org.glue.glue_be.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "group_message")
+@Getter
+@NoArgsConstructor
+public class GroupMessage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupMessageId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "group_chatroom_id")
+    private GroupChatRoom groupChatroom;
+
+    @ManyToOne
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    @Column(name = "message_content")
+    private String message;
+
+    @Column(name = "unread_count")
+    private Integer unreadCount;
+
+    // Constructor with required fields
+    public GroupMessage(User user, GroupChatRoom groupChatroom, Meeting meeting, String message, Integer unreadCount) {
+        this.user = user;
+        this.groupChatroom = groupChatroom;
+        this.meeting = meeting;
+        this.message = message;
+        this.unreadCount = unreadCount;
+    }
+
+    // If message editing is allowed
+    public void updateMessage(String newMessage) {
+        this.message = newMessage;
+    }
+}
