b3c9213a80ca2fc35c88e64e69f2f52b52d0e5d2
Lee Si Woong <silverttthin@naver.com>
2025-05-16 20:51:07 +0900

    refactor: signin req dto를 record 문법으로 변경 - record는 dto 만들 떄 좋다 1. getter 및 생성자 내장 2. 불변 객체 보장 -> 데이터 일관성 확보

--- src/main/java/org/glue/glue_be/auth/dto/request/KakaoSignInRequestDto.java
+++ src/main/java/org/glue/glue_be/auth/dto/request/KakaoSignInRequestDto.java
@@ -2,15 +2,13 @@ package org.glue.glue_be.auth.dto.request;
 
 
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Getter;
 
 
-@Getter
-public class KakaoSignInRequestDto {
+public record KakaoSignInRequestDto (
 
 	@NotEmpty(message = "카카오 발급 토큰값은 필수입니다")
-	String kakaoToken;
-  
-  String fcmToken;
+	String kakaoToken,
 
-}
+    String fcmToken
+
+) {}
