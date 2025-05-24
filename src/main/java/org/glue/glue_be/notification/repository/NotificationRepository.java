501977ff8884e27f94c45299ed46aeb294091939
Gahyeon <best11gh@naver.com>
2025-05-24 16:58:11 +0900

    feat: repository 어노테이션 추가

--- src/main/java/org/glue/glue_be/notification/repository/NotificationRepository.java
+++ src/main/java/org/glue/glue_be/notification/repository/NotificationRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import org.glue.glue_be.notification.entity.Notification;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findByReceiver_UserIdOrderByNotificationIdDesc(Long userId, Pageable pageable);
