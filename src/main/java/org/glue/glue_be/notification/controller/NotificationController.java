83d4d79a106f28a33dc6627cd8b18ee973cad02d
Gahyeon <best11gh@naver.com>
2025-05-24 15:38:18 +0900

    feat: 알림 관련 api + 적용

---
+++ src/main/java/org/glue/glue_be/notification/controller/NotificationController.java
@@ -0,0 +1,46 @@
+package org.glue.glue_be.notification.controller;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.glue.glue_be.auth.jwt.CustomUserDetails;
+import org.glue.glue_be.common.response.BaseResponse;
+import org.glue.glue_be.notification.dto.request.CreateNotificationRequest;
+import org.glue.glue_be.notification.dto.response.NotificationResponse;
+import org.glue.glue_be.notification.service.NotificationService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notification")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    // TODO: 테스트 용도, 개발 완료 후 삭제 예정
+    @PostMapping
+    public BaseResponse<Void> create(@Valid @RequestBody CreateNotificationRequest request) {
+        notificationService.create(request);
+        return new BaseResponse<>();
+    }
+
+    @GetMapping
+    public BaseResponse<NotificationResponse[]> getNotifications(
+            @AuthenticationPrincipal CustomUserDetails auth,
+            @RequestParam(required = false) Long cursorId,
+            @RequestParam(defaultValue = "10") Integer pageSize
+    ) {
+        NotificationResponse[] notifications = notificationService.getNotifications(auth.getUserId(), cursorId,
+                pageSize);
+        return new BaseResponse<>(notifications);
+    }
+
+    @DeleteMapping("/{notificationId}")
+    public BaseResponse<Void> delete(@PathVariable Long notificationId,
+                                     @AuthenticationPrincipal CustomUserDetails auth) {
+        notificationService.delete(notificationId, auth.getUserId());
+        return new BaseResponse<>();
+    }
+
+}
---
+++ src/main/java/org/glue/glue_be/notification/repository/NotificationRepository.java
@@ -0,0 +1,16 @@
+package org.glue.glue_be.notification.repository;
+
+import java.util.List;
+import org.glue.glue_be.notification.entity.Notification;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByReceiver_UserIdOrderByNotificationIdDesc(Long userId, Pageable pageable);
+
+    List<Notification> findByReceiver_UserIdAndNotificationIdLessThanOrderByNotificationIdDesc(Long userId,
+                                                                                               Long cursorId,
+                                                                                               Pageable pageable);
+
+}
