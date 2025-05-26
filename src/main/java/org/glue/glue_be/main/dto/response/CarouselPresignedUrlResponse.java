aafdf2cf768c4d0d6190b4ab452d4b794457e51d
kim-minkyoung <decomin02@pusan.ac.kr>
2025-05-26 16:03:24 +0900

    chore: 불필요 주석 제거

--- src/main/java/org/glue/glue_be/main/dto/response/CarouselPresignedUrlResponse.java
+++ src/main/java/org/glue/glue_be/main/dto/response/CarouselPresignedUrlResponse.java
@@ -2,14 +2,12 @@ package org.glue.glue_be.main.dto.response;
 
 import org.glue.glue_be.aws.dto.GetPresignedUrlResponse;
 
-// Presigned URL 응답 DTO (기존 GetPresignedUrlResponse 활용)
 public record CarouselPresignedUrlResponse(
         String presignedUrl,
         String publicUrl,
         String fileName,
         Long carouselId
 ) {
-    // 기존 GetPresignedUrlResponse에서 변환하는 정적 메서드
     public static CarouselPresignedUrlResponse fromEntity(GetPresignedUrlResponse response, String fileName, Long carouselId) {
         return new CarouselPresignedUrlResponse(
                 response.getPresignedUrl(),
