b7c5ec0e9ce1beeb2270bd1276bbcc69a2d56195
Gahyeon <best11gh@naver.com>
2025-05-27 20:23:11 +0900

    feat: 인증 예외 경로 추가 및 적용

--- src/main/java/org/glue/glue_be/setting/SecurityConfig.java
+++ src/main/java/org/glue/glue_be/setting/SecurityConfig.java
@@ -20,51 +20,51 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig {
 
-	private final JwtAuthenticationFilter jwtAuthenticationFilter;
-	private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
-	private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
 
-	// 인증과정 배제 경로 -> 앞으로추가해나가야함
-	private static final String[] AUTH_WHITELIST = {
-		"/api/auth/kakao/signup"
-	};
+    // 인증과정 배제 경로 -> 앞으로추가해나가야함
+    private static final String[] AUTH_WHITELIST = {
+            "/api/auth/**",
+            "/swagger-ui/**",
+            "/api-docs/**"
+    };
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-			.csrf(AbstractHttpConfigurer::disable)
-			.formLogin(form -> form.disable()) // 폼 로그인 비활성화
-			.sessionManagement(session -> {
-				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-			})
-			.exceptionHandling(exception -> {
-				exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint);
-				exception.accessDeniedHandler(customAccessDeniedHandler);
-			})
-		;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 비활성화
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .exceptionHandling(exception -> {
+                    exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint);
+                    exception.accessDeniedHandler(customAccessDeniedHandler);
+                })
+        ;
 
-		http.authorizeHttpRequests(auth -> {
-			auth.requestMatchers(AUTH_WHITELIST).permitAll();
-//			auth.anyRequest().authenticated();
-			auth.anyRequest().permitAll(); // todo: 개발단계에선 일단 모든 경로에 허용, 원랜 화이트리스트의 경로만 적용해야함
-			})
-			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(AUTH_WHITELIST).permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+        return http.build();
+    }
 
-		return http.build();
-	}
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.addAllowedOriginPattern("*");
-		configuration.addAllowedMethod("*");
-		configuration.addAllowedHeader("*");
-		configuration.setAllowCredentials(true);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
