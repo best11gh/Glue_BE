0cd29c05048f057cfe37a5d000e97cc37ca45880
Gahyeon <best11gh@naver.com>
2025-05-30 12:21:14 +0900

    feat: JWT 인증 로직에 사용자 역할 추가

--- src/main/java/org/glue/glue_be/auth/jwt/CustomUserDetails.java
+++ src/main/java/org/glue/glue_be/auth/jwt/CustomUserDetails.java
@@ -1,44 +1,57 @@
 package org.glue.glue_be.auth.jwt;
 
+import java.util.List;
+import org.glue.glue_be.user.entity.UserRole;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
 
 
 // Principal에 들어갈 유저 정보를 담고있는 객체
 public class CustomUserDetails implements UserDetails {
 
-	// 인증 객체에 들어갈 유저 정보들
-	private final Long userId;
-	private final String userNickName;
+    // 인증 객체에 들어갈 유저 정보들
+    private final Long userId;
+    private final String userNickName;
+    private final UserRole userRole;
 
-	public CustomUserDetails(Long userId, String userNickname) {
-		this.userId = userId;
-		this.userNickName = userNickname;
-	}
+    public CustomUserDetails(Long userId, String userNickname, UserRole userRole) {
+        this.userId = userId;
+        this.userNickName = userNickname;
+        this.userRole = userRole;
+    }
 
-	// Spring Security가 내부적으로 사용하는 용도
-	// 인터페이스 메서드는 getUsername이지만 실제로는 userId를 문자열로 리턴
-	@Override
-	public String getUsername() {
-		return String.valueOf(userId);
-	}
+    // Spring Security가 내부적으로 사용하는 용도
+    // 인터페이스 메서드는 getUsername이지만 실제로는 userId를 문자열로 리턴
+    @Override
+    public String getUsername() {
+        return String.valueOf(userId);
+    }
 
-	// 실제 비즈니스 로직에서 사용할 것
-	public Long getUserId() { return userId; }
+    // 실제 비즈니스 로직에서 사용할 것
+    public Long getUserId() {
+        return userId;
+    }
 
-	public String getUserNickname() { return userNickName; }
+    public String getUserNickname() {
+        return userNickName;
+    }
 
-	// 권한 로직이 아직 존재하지 않으므로 빈 리스트로 처리
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() { return Collections.emptyList(); }
+    public UserRole getRole() {
+        return userRole;
+    }
 
-	// 소셜 로그인 등으로 비밀번호를 쓰지 않을 경우 빈 문자열 리턴
-	@Override
-	public String getPassword() {
-		return "";
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.getAuthority()));
+    }
+
+    // 소셜 로그인 등으로 비밀번호를 쓰지 않을 경우 빈 문자열 리턴
+    @Override
+    public String getPassword() {
+        return "";
+    }
 
 }
--- src/main/java/org/glue/glue_be/auth/jwt/JwtTokenProvider.java
+++ src/main/java/org/glue/glue_be/auth/jwt/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.glue.glue_be.user.entity.UserRole;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,100 +23,109 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-	private static final String MEMBER_ID = "memberId";
-	private static final String MEMBER_NICKNAME = "memberNickname";
-	private static final Long TOKEN_EXPIRATION_TIME = 100 * 24 * 60 * 60 * 1000L; // todo: 토큰 유효기간 배포 시 원상복구 시키기
+    private static final String MEMBER_ID = "memberId";
+    private static final String MEMBER_NICKNAME = "memberNickname";
+    private static final String MEMBER_ROLE = "memberRole";
+    private static final Long TOKEN_EXPIRATION_TIME = 100 * 24 * 60 * 60 * 1000L; // todo: 토큰 유효기간 배포 시 원상복구 시키기
 
 
-	@Value("${jwt.secret}")
-	private String JWT_SECRET;
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
 
 
-	// 앱 실행 직전 jwt secret 키를 주입받은 후 base64로 인코딩하는 작업
-	// -> jjwt 라이브러리는 토큰이 base64 인코딩 문자열로 받아야해서 하는 작업
-	@PostConstruct // 해당 빈이 주입된 후 추가적인 초기화 작업할때 붙이는 어노테이션
-	protected void init() {
-		// base64 라이브러리에서 encodeToString을 이용해 byte[] -> String 타입으로 변환
-		JWT_SECRET = Base64.getEncoder().encodeToString(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
-	}
+    // 앱 실행 직전 jwt secret 키를 주입받은 후 base64로 인코딩하는 작업
+    // -> jjwt 라이브러리는 토큰이 base64 인코딩 문자열로 받아야해서 하는 작업
+    @PostConstruct // 해당 빈이 주입된 후 추가적인 초기화 작업할때 붙이는 어노테이션
+    protected void init() {
+        // base64 라이브러리에서 encodeToString을 이용해 byte[] -> String 타입으로 변환
+        JWT_SECRET = Base64.getEncoder().encodeToString(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+    }
 
-	public String generateToken(Authentication authentication) {
+    public String generateToken(Authentication authentication) {
 
-		CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-		final Date now = new Date();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        final Date now = new Date();
 
-		// 1. 클레임 생성. jwt의 claims엔 여러개가 있는데 우선 토큰 발급시각과 토큰 만료시간 지정
-		final Claims claims = Jwts.claims()
-			.setIssuedAt(now)
-			.setExpiration(new Date(now.getTime() + TOKEN_EXPIRATION_TIME));
+        // 1. 클레임 생성. jwt의 claims엔 여러개가 있는데 우선 토큰 발급시각과 토큰 만료시간 지정
+        final Claims claims = Jwts.claims()
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + TOKEN_EXPIRATION_TIME));
 
-		// 2. userId와 nickname을 JWT 클레임에 삽입
-		claims.put(MEMBER_ID, userDetails.getUserId());
-		claims.put(MEMBER_NICKNAME, userDetails.getUserNickname());
+        // 2. userId와 nickname, role을 JWT 클레임에 삽입
+        claims.put(MEMBER_ID, userDetails.getUserId());
+        claims.put(MEMBER_NICKNAME, userDetails.getUserNickname());
+        claims.put(MEMBER_ROLE, userDetails.getRole());
 
-		// 3. header, 방금 조립한 claim, 그리고 signature을 합쳐 JWT 빌드
-		return Jwts.builder()
-			.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-			.setClaims(claims)
-			.signWith(getSigningKey())
-			.compact();
-	}
-
-
-	// 서명 생성
-	private Key getSigningKey() {
-		// 1. jwt 비밀 키를 인코드 한 값을 가져옴
-		String encodedKey = Base64.getEncoder().encodeToString(JWT_SECRET.getBytes());
-
-		// 2. 주어진 키 값을 기반으로 HMAC-SHA 암호화 알고리즘에 쓰이는 Key 객체 리턴
-		return Keys.hmacShaKeyFor(encodedKey.getBytes());
-	}
-
-	public JwtValidationType validateToken(String token) {
-		try {
-			final Claims claims = getBody(token);
-			return JwtValidationType.VALID_JWT;
-		} catch (MalformedJwtException ex) {
-			return JwtValidationType.INVALID_JWT_TOKEN;
-		} catch (ExpiredJwtException ex) {
-			return JwtValidationType.EXPIRED_JWT_TOKEN;
-		} catch (UnsupportedJwtException ex) {
-			return JwtValidationType.UNSUPPORTED_JWT_TOKEN;
-		} catch (IllegalArgumentException ex) {
-			return JwtValidationType.EMPTY_JWT;
-		}
-	}
+        // 3. header, 방금 조립한 claim, 그리고 signature을 합쳐 JWT 빌드
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setClaims(claims)
+                .signWith(getSigningKey())
+                .compact();
+    }
 
 
-	// claim 파싱 함수
-	private Claims getBody(final String token) {
-		return Jwts.parserBuilder()
-			.setSigningKey(getSigningKey())
-			.build()
-			.parseClaimsJws(token)
-			.getBody();
-	}
+    // 서명 생성
+    private Key getSigningKey() {
+        // 1. jwt 비밀 키를 인코드 한 값을 가져옴
+        String encodedKey = Base64.getEncoder().encodeToString(JWT_SECRET.getBytes());
 
-	// claim에서 유저 인증주체 정보 파싱 메서드
-	public Long getUserIdFromJwt(String token) {
-		Claims claims = getBody(token);
-		return Long.valueOf(claims.get(MEMBER_ID).toString());
-	}
+        // 2. 주어진 키 값을 기반으로 HMAC-SHA 암호화 알고리즘에 쓰이는 Key 객체 리턴
+        return Keys.hmacShaKeyFor(encodedKey.getBytes());
+    }
 
-	public String getUserNicknameFromJwt(String token) {
-		Claims claims = getBody(token);
-		return claims.get(MEMBER_NICKNAME).toString();
-	}
+    public JwtValidationType validateToken(String token) {
+        try {
+            final Claims claims = getBody(token);
+            return JwtValidationType.VALID_JWT;
+        } catch (MalformedJwtException ex) {
+            return JwtValidationType.INVALID_JWT_TOKEN;
+        } catch (ExpiredJwtException ex) {
+            return JwtValidationType.EXPIRED_JWT_TOKEN;
+        } catch (UnsupportedJwtException ex) {
+            return JwtValidationType.UNSUPPORTED_JWT_TOKEN;
+        } catch (IllegalArgumentException ex) {
+            return JwtValidationType.EMPTY_JWT;
+        }
+    }
 
-	public Authentication getAuthentication(String token) {
-		Long userId = getUserIdFromJwt(token);
-		String userNickname = getUserNicknameFromJwt(token);
 
-		// 사용자 정보를 기반으로 UserDetails 객체 생성
-		CustomUserDetails userDetails = new CustomUserDetails(userId, userNickname);
+    // claim 파싱 함수
+    private Claims getBody(final String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
-		// 인증 객체 생성 및 반환
-		return new UsernamePasswordAuthenticationToken(
-				userDetails, "", Collections.emptyList());
-	}
+    // claim에서 유저 인증주체 정보 파싱 메서드
+    public Long getUserIdFromJwt(String token) {
+        Claims claims = getBody(token);
+        return Long.valueOf(claims.get(MEMBER_ID).toString());
+    }
+
+    public String getUserNicknameFromJwt(String token) {
+        Claims claims = getBody(token);
+        return claims.get(MEMBER_NICKNAME).toString();
+    }
+
+    public UserRole getUserRoleFromJwt(String token) {
+        Claims claims = getBody(token);
+        return UserRole.valueOf(claims.get(MEMBER_ROLE).toString());
+    }
+
+
+    public Authentication getAuthentication(String token) {
+        Long userId = getUserIdFromJwt(token);
+        String userNickname = getUserNicknameFromJwt(token);
+        UserRole role = getUserRoleFromJwt(token);
+
+        // 사용자 정보를 기반으로 UserDetails 객체 생성
+        CustomUserDetails userDetails = new CustomUserDetails(userId, userNickname, role);
+
+        // 인증 객체 생성 및 반환
+        return new UsernamePasswordAuthenticationToken(
+                userDetails, "", userDetails.getAuthorities());
+    }
 }
