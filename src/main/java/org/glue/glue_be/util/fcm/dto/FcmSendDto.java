d79bd50823c937d22f8a349218eeb74f7f4ac188
Gahyeon <best11gh@naver.com>
2025-06-11 16:46:28 +0900

    feat: FCM 알림에 type, id, guestbookHostId 필드 추가 및 전송 구조 개선

--- src/main/java/org/glue/glue_be/util/fcm/dto/FcmSendDto.java
+++ src/main/java/org/glue/glue_be/util/fcm/dto/FcmSendDto.java
@@ -6,15 +6,20 @@ import lombok.*;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FcmSendDto {
-        private String token;
-        private String title;
-        private String body;
+    private String token;
+    private String title;
+    private String body;
+    private String type;           // 알림 타입 guestbook, post, notice, dm, group 중 하나
+    private Long id;               // targetId
+    private Long guestbookHostId;  // guestbook 타입일 때만 사용
 
-        @Builder(toBuilder = true)
-        public FcmSendDto(String token, String title, String body) {
-                this.token = token;
-                this.title = title;
-                this.body = body;
-        }
+    @Builder(toBuilder = true)
+    public FcmSendDto(String token, String title, String body, String type, Long id, Long guestbookHostId) {
+        this.token = token;
+        this.title = title;
+        this.body = body;
+        this.type = type;
+        this.id = id;
+        this.guestbookHostId = guestbookHostId;
+    }
 }
-
